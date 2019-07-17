11456571d17473d9336be23824482a933a9e7575
Ray Thurman <raymondthurman5@gmail.com>
2019-07-17 07:48:02 -0400

    some css and js snippets

--- javascript/main.js
+++ javascript/main.js
@@ -2,3 +2,16 @@
 const randomNumberInRange = (min, max) => Math.random() * (max - min) + min;
 
 console.log(randomNumberInRange(2, 10));
+
+// LongestItem
+/*   Takes any number of iterable objects wit ha length property and returns the longest one.   */
+const longestItem = (...vals) => vals.reduce((a, x) => (x.length > a.length ? x : a));
+
+console.log(longestItem('this', 'is', 'a', 'testcase'));
+
+// averageBy
+// Returns the average of an array
+const averageBy = (arr, fn) =>
+	arr.map(typeof fn === 'function' ? fn : (val) => val[fn]).reduce((acc, val) => acc + val, 0) / arr.length;
+
+console.log(averageBy([ { n: 4 }, { n: 2 }, { n: 8 }, { n: 6 } ], (o) => o.n));
