c590997e8328ab681f34136b8f6e7549509fb478
Ray Thurman <raymondthurman5@gmail.com>
2019-07-24 10:04:46 -0400

    JS snippets and React lifecycle methods

--- javascript/main.js
+++ javascript/main.js
@@ -15,3 +15,18 @@ const averageBy = (arr, fn) =>
 	arr.map(typeof fn === 'function' ? fn : (val) => val[fn]).reduce((acc, val) => acc + val, 0) / arr.length;
 
 console.log(averageBy([ { n: 4 }, { n: 2 }, { n: 8 }, { n: 6 } ], (o) => o.n));
+
+// isFunction
+const isFunction = (val) => typeof val === 'function';
+
+console.log(isFunction('x'));
+console.log(isFunction((x) => x));
+
+// tomorrow
+const tomorrow = () => {
+	let t = new Date();
+	t.setDate(t.getDate() + 1);
+	return t.toISOString().split('T')[0];
+};
+
+console.log(tomorrow());
