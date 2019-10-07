4a214aeb1d1d147af06d59b77a043be9b0d2f609
Ray Thurman <raymondthurman5@gmail.com>
2019-10-07 09:49:17 -0400

    refactored the miniMax problem

--- javascript/algorithms.js
+++ javascript/algorithms.js
@@ -37,7 +37,7 @@ const plusMinus = (arr) => {
 // Mini-Max Sum
 // Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.Then print the respective minimum and maximum values as a single line of two space - separated long integers.
 
-// let array = [ 3, 4, 5, 1, 2 ];
+let array = [ 3, 4, 5, 1, 2 ];
 
 const miniMaxSum = (arr) => {
 	// Make sure the array is sorted
@@ -45,14 +45,17 @@ const miniMaxSum = (arr) => {
 	// grab the lowest 4 items
 	let lowestArr = sortedArr.slice(0, 4);
 	// grab the highest 4 items
-	let lowItem = arr.length - 4;
-	let highItem = arr.length;
-	let highestArr = sortedArr.slice(lowItem, highItem);
-
+	let highestArr = sortedArr.slice(arr.length - 4, arr.length);
 	// Loop over arrays and add the numbers
 	let lowest = lowestArr.reduce((acc, num) => acc + num);
 	let highest = highestArr.reduce((acc, num) => acc + num);
 	console.log(lowest, highest);
 };
 
-// miniMaxSum(array);
+miniMaxSum(array);
+
+// Birthday Cake Candles
+// You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
+
+// For example, if your niece is turning
+// years old, and the cake will have candles of height, , , , she will be able to blow out candles successfully, since the tallest candles are of height and there are such candles.
