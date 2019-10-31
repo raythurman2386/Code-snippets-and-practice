e631d9113c676ec95c96a660f8ad4562255106fd
raythurman2386 <raymondthurman5@gmail.com>
2019-10-30 21:22:12 -0400

    added consolelog

--- javascript/algorithms.js
+++ javascript/algorithms.js
@@ -3,10 +3,12 @@
 
 let total = 0;
 
-for (let i = 1; i < 100; i << 1) {
+for (let i = 1; i < 5; i << 1) {
   console.log(i);
   total += i;
 }
+
+console.log(total);
 
 // Plus Minus
 const plusMinus = arr => {
