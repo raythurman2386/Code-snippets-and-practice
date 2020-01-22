10d1974b9e9faab557423042e3bb1e121b1743d2
Ray Thurman <raymondthurman5@gmail.com>
2020-01-22 11:02:16 -0500

    extra long factorial algorithm

--- javascript/algorithms.js
+++ javascript/algorithms.js
@@ -149,18 +149,77 @@ primeList(2, 30);
 */
 
 function diagonalDifference(arr) {
-   // Write your code here
-   let primary = 0;
-   let secondary = 0;
-   // we know the 2d matrix is a square so the length of the array
-   // is also the length of the inside arrays
-   const {length} = arr
-   let x = 0; // beginning of array
-   let y = length - 1; // end of array
-   for (let i = 0; i < length; i++) {
-       primary += arr[i][x+i]
-       secondary += arr[i][y-i]
-   }
+  // Write your code here
+  let primary = 0;
+  let secondary = 0;
+  // we know the 2d matrix is a square so the length of the array
+  // is also the length of the inside arrays
+  const { length } = arr
+  let x = 0; // beginning of array
+  let y = length - 1; // end of array
+  for (let i = 0; i < length; i++) {
+    primary += arr[i][x + i]
+    secondary += arr[i][y - i]
+  }
 
-   return Math.abs(primary - secondary)
+  return Math.abs(primary - secondary)
+}
+
+// Largest factorial
+function extraLongFactorials(n) {
+  let fact = 1;
+
+  function add(str1, str2) {
+
+    let sum = "";  // our result will be stored in a string.
+
+    // we'll need these in the program many times.
+    let str1Length = str1.length;
+    let str2Length = str2.length;
+
+    // if s2 is longer than s1, swap them.
+    if (str2Length > str1Length) {
+      let temp = str2;
+      str2 = str1;
+      str1 = temp;
+    }
+
+    let carry = 0;  // number that is carried to next decimal place, initially zero.
+    let a;
+    let b;
+    let temp;
+    let digitSum;
+    for (let i = 0; i < str1.length; i++) {
+      a = parseInt(str1.charAt(str1.length - 1 - i));      // get ith digit of str1 from right, we store it in a
+      b = parseInt(str2.charAt(str2.length - 1 - i));      // get ith digit of str2 from right, we store it in b
+      b = (b) ? b : 0;                                    // make sure b is a number, (this is useful in case, str2 is shorter than str1
+      temp = (carry + a + b).toString();                  // add a and b along with carry, store it in a temp string.
+      digitSum = temp.charAt(temp.length - 1);            //
+      carry = parseInt(temp.substr(0, temp.length - 1));  // split the string into carry and digitSum ( least significant digit of abSum.
+      carry = (carry) ? carry : 0;                        // if carry is not number, make it zero.
+
+      sum = (i === str1.length - 1) ? temp + sum : digitSum + sum;  // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
+
+    }
+
+    return sum;     // return sum
+
+  }
+
+  for (let i = 2; i <= n; i++) {
+
+    if (Number.isSafeInteger(fact * i)) {
+      fact = fact * i;
+    }
+    else {
+      //fact = fact + fact + .. i times
+      let factxi = "0";  // this is (fact * i) for us.
+      for (let j = 0; j < i; j++) {
+        factxi = add(factxi, fact.toString());
+      }
+      fact = factxi; // update value of fact before continuing the loop.
+    }
+  }
+
+  console.log(fact);
 }
